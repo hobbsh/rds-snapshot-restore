fa86b59d54bbfa56ecb32c7fcbbb30204be6fc0b
Wylie Hobbs <wylie@wyliehobbs.com>
2018-07-24 15:32:53 -0600

    Add quick/dirty docker build script

--- docker_build.sh
+++ docker_build.sh
@@ -7,7 +7,7 @@ REPO="wylie"
 GIT_SHA=$(git rev-parse --short HEAD)
 NEW_IMAGE="$REPO/$APP:$GIT_SHA"
 
-docker build -t rds-snapshot-restore .
+docker build -t $APP .
 
 if [ $? -eq 0 ];
   IMAGE_ID=$(docker images | grep -E ^$APP | awk '{print $3}')
