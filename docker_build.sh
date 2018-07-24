0f170c9fcc1d61c2d6ef4811ba928e0937c4061a
Wylie Hobbs <wylie@wyliehobbs.com>
2018-07-24 15:32:26 -0600

    Add quick/dirty docker build script

---
+++ docker_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Build the docker image
+
+APP="rds-snapshot-restore"
+REPO="wylie"
+GIT_SHA=$(git rev-parse --short HEAD)
+NEW_IMAGE="$REPO/$APP:$GIT_SHA"
+
+docker build -t rds-snapshot-restore .
+
+if [ $? -eq 0 ];
+  IMAGE_ID=$(docker images | grep -E ^$APP | awk '{print $3}')
+
+  docker tag $IMAGE_ID $NEW_IMAGE
+  docker push $NEW_IMAGE
+else
+  echo "Someting went wrong building the image. Exiting"
+  exit 1
+fi
